d128ac906490f7cacfcfe732de043fc5bc9c3a72
angelfernandezg <angel.f.g@opendeusto.es>
2025-02-17 14:03:00 +0100

    quickchanges

--- xss_project/xss_app/views.py
+++ xss_project/xss_app/views.py
@@ -8,8 +8,8 @@ sanitizer = Sanitizer()
 def xss_vulnerable(request):
     if request.method == 'POST':
         user_input = request.POST.get('comment', '')
-        return render(request, 'C:/Users/angel/xss_project/xss_app/templates/xss_result.html', {'result': user_input, 'sanitized': False})
-    return render(request, 'C:/Users/angel/xss_project/xss_app/templates/xss_form.html')
+        return render(request, 'C:/Users/angel/XSS_para_sistemas/xss_project/xss_app/templates/xss_result.html', {'result': user_input, 'sanitized': False})
+    return render(request, 'C:/Users/angel/XSS_para_sistemas/xss_project/xss_app/templates/xss_form.html')
 
 def xss_protected(request):
     if request.method == 'POST':
