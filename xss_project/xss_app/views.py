d5ac91a1270adbd07eda306e45fc784a62784301
angelfernandezg <angel.f.g@opendeusto.es>
2025-02-19 14:50:49 +0100

    bonito

--- xss_project/xss_app/views.py
+++ xss_project/xss_app/views.py
@@ -8,12 +8,12 @@ sanitizer = Sanitizer()
 def xss_vulnerable(request):
     if request.method == 'POST':
         user_input = request.POST.get('comment', '')
-        return render(request, 'xss_result.html', {'result': user_input, 'sanitized': False})
-    return render(request, 'xss_form.html')
+        return render(request, 'xss_result.html', {'result': user_input, 'sanitized': False, 'css_file': 'xss_app/vulnerable.css'})
+    return render(request, 'xss_form.html', {'css_file': 'xss_app/vulnerable.css'})
 
 def xss_protected(request):
     if request.method == 'POST':
         user_input = request.POST.get('comment', '')
-        sanitized_input = sanitizer.sanitize(user_input)
-        return render(request, 'xss_result.html', {'result': sanitized_input, 'sanitized': True})
-    return render(request, 'xss_form.html')
+        sanitized_input = escape(user_input)
+        return render(request, 'xss_result.html', {'result': sanitized_input, 'sanitized': True, 'css_file': 'xss_app/protected.css'})
+    return render(request, 'xss_form.html', {'css_file': 'xss_app/protected.css'})
